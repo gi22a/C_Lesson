bf164ff989eb0704178119ea44369638f19df591
ilya <arhipov.a2cat@mail.ru>
2023-04-26 10:52:37 +0300

    fist

---
+++ zadacha8/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число для разбора: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = num; 
+
+Console.Write($"{num} = "); 
+
+for(int i = 0; i < num; i++){           
+    if(result %2 ==0){                   
+        Console.Write($"{result}, ");   
+    }                                   
+    result--;                           
+}
